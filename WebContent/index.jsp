0496bf2b506479f6f541bbfb60e170cf2fa1ab3e
ruoyezhiqiu <18380231337@163.com>
2017-01-09 23:29:24 +0800

    update

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -1,12 +1,42 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript" src="scripts/jquery-1.12.3.min.js"></script>
+<script type="text/javascript">
+	$(function(){
+		$("#testJson").click(function(){
+			var url = this.href;
+			var args = {};
+			$.post(url, args, function(data){
+				for(var i = 0; i < data.length; i++){
+					var id = data[i].id;
+					var lastName = data[i].lastName;
+					
+					alert(id + ": " + lastName);
+				}
+			});
+			return false;
+		});
+	})
+</script>
 </head>
 <body>
-	<a href="emps">Add New Employee</a>
+	<a href="emps">Show All Employee</a>
+	<br><br>
+	<a href="testJson" id="testJson">Test Json</a>
+	<br><br>
+	<p>演示文件上传：</p>
+	<br><br>
+	<form action="testHttpMessageConverter" method="POST" enctype="multipart/form-data">
+		File:<input type="file" name="file"/>
+		Desc:<input type="text" name="desc"/>
+		<input type="submit" value="上传"/>
+	</form> 
+	<br><br>
+	<p>演示文件下载：</p><br><br>
+	<a href="testResponseEntity">下载</a>
 </body>
 </html>
--- src/com/wushengde/springmvc/crud/test/SpringmvcTest.java
+++ src/com/wushengde/springmvc/crud/test/SpringmvcTest.java
@@ -1,9 +1,22 @@
 package com.wushengde.springmvc.crud.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wushengde.springmvc.crud.dao.EmployeeDao;
 import com.wushengde.springmvc.crud.entities.Employee;
@@ -12,12 +25,49 @@ import com.wushengde.springmvc.crud.entities.Employee;
 public class SpringmvcTest {
 
 	@Autowired
-	private EmployeeDao emploeeDao;
+	private EmployeeDao employeeDao;
+	
+	
+	//演示文件下载：
+	@RequestMapping("/testResponseEntity")
+	public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException{
+		byte [] body = null;
+		ServletContext servletContext = session.getServletContext();
+		InputStream inputStream=servletContext.getResourceAsStream("/files/abc.txt");
+		body = new byte[inputStream.available()];
+		inputStream.read(body);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "attachment;filename=abc.txt");
+		
+		HttpStatus statusCode = HttpStatus.OK;
+		
+		ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body,headers,statusCode);
+		return response;
+	}
+	
+	
+	//演示文件上传
+	@ResponseBody
+	@RequestMapping("/testHttpMessageConverter")
+	public String testHttpMessageConverter(@RequestBody String body){
+		System.out.println(body);
+		return "helloworld!"+new Date();
+	}
+	
+	
+	//返回Json时用：@ResponseBody 注解
+	@ResponseBody
+	@RequestMapping("/testJson")
+	public Collection<Employee> testJson(){
+		return employeeDao.getAll();
+	}
+	
 	
 	@RequestMapping("/testConversionServiceConverer")
 	public String testConverter(@RequestParam("employee") Employee employee){
 		System.out.println("save: "+employee);
-		emploeeDao.save(employee);
+		employeeDao.save(employee);
 		return "redirect:/emps";
 	}
 	
